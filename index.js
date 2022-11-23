b7b01c9f729acb661087f09918a5b636982040a5
git_manasse8002 <manassemburu@gmail.com>
2022-11-23 13:08:58 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+console.log ("Hello npm")
+const print = (tag,message) => console.log(tag,message)
+print("radaboss:","Hello npm")
+//print hello x5
+//for=loop
+//syntax:for(interation initializer; stopping condition; iteration continue){
+//
+//}
+
+for(let i = 1; i < 6; i = i + 1){
+    print(i,"Hello")
+}
+
+for(let i = 1; i < 6; i = i + 1){
+    print("Hello,=>",i*i)
+}
+let j = 1;
+while(j < 6){
+    print(j,"Wakanda")
+    j += 1 
+
+   
+   // j++  tomean post increament
+   // ++j mean pre increament }
+} 
+let m = 1
+do{
+    print(m, "Hello")
+    m++
+}   while(m<6)
+
+let z = 1
+while(z<1){
+    print("ijkldmnc","gdftrredsg")
+    z++
+
+}
+
+do {
+    print("random","Hello")
+    z++
+}while(z < 1)
